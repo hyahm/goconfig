af9cbd7716192ce6f9ca00b848f2d65d19a261ca
cander <cander_liubiao@126.com>
2019-12-22 09:57:20 +0800

    get

--- example/example.go
+++ example/example.go
@@ -13,6 +13,15 @@ type user struct {
 func main() {
 	// 初始化配置文件
 	goconfig.InitConf("test.conf")
+	//uint64 = 9223372036854775808
+	//int64 = 123456223
+	//int = 1234556223
+	//bool = true
+	//password = "adlfjlskdf "
+	//[one]
+	//float = 0.25
+	//string = goconfig
+	print(goconfig.ReadString("uint64"))
 	// 写入 模块：key, key: name, value: cander, 备注：姓名     的配置文件
 	//goconfig.WriteString("key.name", "cander", "姓名")
 	//// 写入 模块：key, key: name, value: cander, 备注：用户表       的配置文件
@@ -22,5 +31,6 @@ func main() {
 	//	Age: 20,
 	//})
 	//goconfig.WriteBytes("user", send, "用户表")
+	//goconfig.PrintKeyValue()
 
 }
