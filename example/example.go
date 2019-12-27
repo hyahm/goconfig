8773122645296842c4a0634083d1637daaade15a
cander <cander@htdtek.com>
2019-12-27 18:11:50 +0800

    添加配置文件软加载

--- example/example.go
+++ example/example.go
@@ -12,9 +12,9 @@ type user struct {
 
 func main() {
 	// 初始化配置文件
-	goconfig.InitWriteConf("write.conf")  // 与InitConf的区别是， 这个会清空里面原有数据
+	goconfig.InitConf("write.conf") // 与InitConf的区别是， 这个会清空里面原有数据
 	//goconfig.InitConf("test.conf")  // 原有配置文件添加
-	goconfig.WriteString("mysql.host", "127.0.0.1","mysql数据库host")
+	goconfig.WriteString("mysql.host", "127.0.0.1", "mysql数据库host")
 	goconfig.WriteInt("mysql.port", 3306)
 	goconfig.WriteString("mysql.db", "name")
 	goconfig.FlushWrite()
