577dfc7e8a1f87c1da6a95b91bcde3726ae30171
cander <cander_liubiao@126.com>
2020-05-13 14:31:56 +0800

    增加readarrayfromnode

--- example/example.go
+++ example/example.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/hyahm/goconfig"
+	"github.com/hyahm/golog"
 )
 
 type user struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	On             bool   `json:"on"`
+	Key            string `json:"key"`
+	redis_host     string `json:"redis_host"`
+	redis_password string `json:"redis_password"`
+	redis_db       int    `json:"redis_db"`
 }
 
 func main() {
@@ -23,14 +25,22 @@ func main() {
 	if err := goconfig.InitConf("client.ini", goconfig.INI); err != nil {
 		log.Fatal(err)
 	}
-
-	// goconfig.InitConf("write.conf") // 与InitConf的区别是， 这个会清空里面原有数据
+	u5 := make([]*user, 0)
+	err := goconfig.ReadArrayBytes("u5", &u5)
+	if err != nil {
+		golog.Error(err)
+	}
+	for _, v := range u5 {
+		fmt.Printf("%+v", v)
+	}
+	fmt.Println(u5)
+	// // goconfig.InitConf("write.conf") // 与InitConf的区别是， 这个会清空里面原有数据
+	// // goconfig.PrintKeyValue()
+	// fmt.Println(goconfig.ReadBool("u5.redis_download.on"))
+	// fmt.Println(goconfig.ReadDuration("server.timeout"))
+	// fmt.Println(goconfig.ReadString("u5.redis_handle.redis_password"))
+	// l := make([]string, 0)
+	// json.Unmarshal(goconfig.ReadBytes("u5.redis_download.pks"), &l)
 	// goconfig.PrintKeyValue()
-	fmt.Println(goconfig.ReadBool("u5.redis_download.on"))
-	fmt.Println(goconfig.ReadDuration("server.timeout"))
-	fmt.Println(goconfig.ReadString("u5.redis_handle.redis_password"))
-	l := make([]string, 0)
-	json.Unmarshal(goconfig.ReadBytes("u5.redis_download.pks"), &l)
-	goconfig.PrintKeyValue()
-	fmt.Println(l)
+	// fmt.Println(l)
 }
