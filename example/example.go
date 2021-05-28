15a5d94745c36f1b77cd196d95880e7de5fc9b2d
cander <cander_liubiao@126.com>
2021-05-28 16:32:53 +0800

    支持变量定义

--- example/example.go
+++ example/example.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hyahm/goconfig"
+	"github.com/hyahm/golog"
 )
 
 type p struct {
@@ -25,40 +28,11 @@ type user struct {
 	Kps           []string `json:"kps"`
 }
 
-type ttt struct {
-	key   string
-	value string
-	note  []string
-}
-
 func main() {
 	// 初始化配置文件
-
+	defer golog.Sync()
 	goconfig.InitConf("client.ini", goconfig.INI)
-
-	// goconfig.WriteInt("aaa.bbb.ccc", 5)
-
-	// // goconfig.FlushWrite()
-	// if err := goconfig.InitConf("client.ini", goconfig.INI); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// u5 := make([]*user, 0)
-	// err := goconfig.ReadArrayFromNode("u5", &u5)
-	// if err != nil {
-	// 	golog.Error(err)
-	// }
-	// for _, v := range u5 {
-	// 	fmt.Printf("%+v", v)
-	// }
-	// fmt.Println(u5)
-	// // goconfig.InitConf("write.conf") // 与InitConf的区别是， 这个会清空里面原有数据
-	// // goconfig.PrintKeyValue()
-	// fmt.Println(goconfig.ReadBool("u5.redis_download.on"))
-	// fmt.Println(goconfig.ReadDuration("server.timeout"))
-	// fmt.Println(goconfig.ReadString("u5.redis_handle.redis_password"))
-	// l := make([]string, 0)
-	// json.Unmarshal(goconfig.ReadBytes("u5.redis_download.pks"), &l)
-	// goconfig.PrintKeyValue()
-	// fmt.Println(l)
-	// log.Fatal(goconfig.Start())
+	m := goconfig.ReadString("u5.redis_download.key")
+	fmt.Println(m)
+	fmt.Println(goconfig.ReadInt("bbb"))
 }
